11b40bc4880416436b18924d5ecd46aa66f6d6a1
Sarah Kuss <sarah.kuss33@gmail.com>
2023-04-02 15:23:52 -0400

    added class assessment

---
+++ class/assesment.js
@@ -0,0 +1,24 @@
+const assessment = {
+  name: 'Sarah Kuss',
+  age: 27,
+  isFeelingConfident: true
+}
+console.log(assessment)
+
+function addNums (numOne, numTwo) {
+  return numOne + numTwo
+}
+addNums(3, 5)
+
+// const countUp = () => { 
+//   for (let i = 1; i < 11; i++) 
+//   { console.log(i); } } 
+//   countUp()
+
+  let person = {
+    name: 'Todd',
+    age: 46, 
+    awesome: true}
+
+    person.name = 'Albert'
+    console.log(person)
